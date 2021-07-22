d85229a306f747930fde7d8031129548f61b5a8d
Krishna Yadav <krishbeck@yahoo.com>
2021-07-21 23:44:05 -0500

    Add eventListner for submit button
    - Introduced check input method
    - Added css for form in signup_form css

--- validation.js
+++ validation.js
@@ -1,10 +1,23 @@
 
 
 const validateForm = () => {
-    let valid = false;
+
+    const form = document.getElementById("form");
     const email = document.getElementById("email");
     const password = document.getElementById("pass");
     const repeatPassword = document.getElementById("repeatPass");
+
+    form.addEventListener("submit", (e)=>({
+        e.preventDefault();
+
+        checkInput();
+    });
+
+
+    function checkInput(){
+
+    }
 }
+
 
 
