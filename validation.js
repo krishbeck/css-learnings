55db98bcc91b7c08c252caf05d14cf84651ea13a
Krishna Yadav <krishbeck@yahoo.com>
2021-07-21 22:40:41 -0500

    Add validation.js file
     - Added external js file
     - Get form fields
     - Cancel button redirects to Login Form

---
+++ validation.js
@@ -0,0 +1,10 @@
+
+
+const validateForm = () => {
+    let valid = false;
+    const email = document.getElementById("email");
+    const password = document.getElementById("pass");
+    const repeatPassword = document.getElementById("repeatPass");
+}
+
+
