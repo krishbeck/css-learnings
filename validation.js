0d64bbffd9321eb398ce212638d66f7bc3744a35
Krishna Yadav <krishbeck@yahoo.com>
2021-07-22 23:44:36 -0500

    Add validation for email (empty check)
    - Prepared function for email validation
    - Prepared and display email based email message
    - Added styling for error message

--- validation.js
+++ validation.js
@@ -1,23 +1,56 @@
-
-
+/**
+ * Validates form
+ */
 const validateForm = () => {
+    const form = document.getElementById("registrationForm");
+    const emailElement = document.getElementById("email");
+    const password = document.getElementById("psw");
+    const repeatPassword = document.getElementById("psw-repeat");
 
-    const form = document.getElementById("form");
-    const email = document.getElementById("email");
-    const password = document.getElementById("pass");
-    const repeatPassword = document.getElementById("repeatPass");
+    // 1. verify email
+    verifyEmail(emailElement);
 
-    form.addEventListener("submit", (e)=>({
-        e.preventDefault();
+    // TODO :: 2. verify password
+    // 2. a. verify password is at least 6 character long
+    // 2. b. verify password has at least a number and capital and symbol
+    // 2. c. verify both password are same
 
-        checkInput();
-    });
+    // TODO :: 3. prevent default, if error
 
-
-    function checkInput(){
-
-    }
+    // TODO :: 4. success message, if inputs are okay
 }
 
+/**
+ * Verifies email
+ *
+ * @param emailElement email HTML element
+ */
+function verifyEmail(emailElement) {
+    const email = emailElement.value.trim();
 
+    const isEmailEmpty = (email) => {
+        return email === "";
+    }
+
+    const isEmailInCorrectFormat = (email) => {
+        // check if email is incorrect format using regex
+        return false; // TODO :: add logic
+    }
+
+    let isError = false;
+    let errorMessage = "";
+
+    if (isEmailEmpty(email)) {                              // 1. check if empty
+        isError = true;
+        errorMessage = "Email can not be blank";
+    } else if (!isEmailInCorrectFormat(email)) {            // 2. check if correct email format
+        isError = true;
+        errorMessage = "Email is not in correct format";
+    }
+
+    if (isError) {
+        const emailErrorDiv = document.getElementById("emailError");
+        emailErrorDiv.innerText = errorMessage;
+    }
+}
 
